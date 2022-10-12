6b0df91df8dd5c8b5bd8f02c9f5aac397db04da7
Bolio <again.bolio@hotmail.com>
2022-10-12 18:20:54 -0500

    Add route for the Dashboard component (without useToken).

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import SignIn from "./components/SignIn";
+import Dashboard from "./components/Dashboard";
 
 function App() {
+  const useToken = useSelector((state) => state.token);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignIn />}>
-           {/* <Route path="tasks" element={<Inicio />} /> */}
-        </Route>
+        <Route path="/" element={<SignIn />} />
+        {/* {useToken && <Route path="/dashboard" element={<Dashboard />} />} */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
