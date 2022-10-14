c1bc2bdc920896f6586caaa48728593321649b8e
Bolio <again.bolio@hotmail.com>
2022-10-14 12:40:49 -0500

    Use PrivateRoute component in App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  const useToken = useSelector((state) => state.token);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
-        {/* {useToken && <Route path="/dashboard" element={<Dashboard />} />} */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
