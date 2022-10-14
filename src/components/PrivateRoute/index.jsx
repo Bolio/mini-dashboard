418d38b610c6dd3a035cf484f2bbe2851c7348cb
Bolio <again.bolio@hotmail.com>
2022-10-14 12:56:12 -0500

    Use NotFound component.

--- src/components/PrivateRoute/index.jsx
+++ src/components/PrivateRoute/index.jsx
@@ -1,11 +1,12 @@
 // import { useSelector } from "react-redux";
+import NotFound from "../NotFound";
 
 const PrivateRoute = ({ children }) => {
   // const useToken = useSelector((state) => state.token);
   // console.log("useToken ---BOLIO :", useToken);
   const useAccessToken = sessionStorage.getItem("accessToken");
 
-  return useAccessToken ? children : "";
+  return useAccessToken ? children : <NotFound />;
 };
 
 export default PrivateRoute;
