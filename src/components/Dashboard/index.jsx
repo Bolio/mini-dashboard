90c3710af04602624ea13dce08fd28a78bb39c37
Bolio <again.bolio@hotmail.com>
2022-10-13 21:17:05 -0500

    Add styles to the Logout button.

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -71,17 +71,26 @@ const Dashboard = () => {
 
   return (
     <div className="container-fluid">
-      <h2>{`Bienvenido ${userName}`}</h2>
-      <p>
-        Reporte de <b>Hoy</b>
-      </p>
-      <button
-        class="btn btn-outline-secondary"
-        type="button"
-        onClick={() => handleLogout()}
-      >
-        Logout
-      </button>
+      <div className="row">
+        <div className="col-md-6">
+          <h2>{`Bienvenido ${userName}`}</h2>
+          <p>Reporte de <b>Hoy</b></p>
+        </div>
+        <div className="col-md-6 text-end">
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={() => handleLogout()}
+          >
+            Logout
+            <p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5z"/>
+              </svg>
+            </p>
+          </button>
+        </div>
+      </div>
       <div className="row align-items-md-stretch">
         <div className="col-md-6">
           <div className="h-100 p-5 bg-light border rounded-3">
@@ -89,7 +98,6 @@ const Dashboard = () => {
             <h5>
               <b>{`$${revenue ? revenue : '00'}.00`}</b> MXN
             </h5>
-            {/* CHART */}
             <LineChart dataChart={revenuePerHour} />
           </div>
         </div>
